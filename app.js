2a1db303b37ec78efcf6d09132b59abda05e46ab
mikehwagz <hmiklw@me.com>
2016-12-15 14:03:33 -0500

    style search bar and add clear button

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', _ => {
 
   const form = document.querySelector('.search');
-  const input = document.querySelector('input[type="search"]');
+  const input = document.querySelector('.search__input');
+  const clear = document.querySelector('.search__clear');
 
   form.addEventListener('submit', searchThis);
+
+  input.addEventListener('keyup', _ => {
+    input.value.length > 0 ? show(clear) : hide(clear);
+  })
+
+  clear.addEventListener('click', _ => {
+    input.value = '';
+    hide(clear)
+  });
 
   function searchThis(e) {
     const query = input.value
@@ -12,6 +22,14 @@ document.addEventListener('DOMContentLoaded', _ => {
     chrome.tabs.update(null, {
         url: url
     });
+  }
+
+  function show(el) {
+    el.classList.remove('hidden');
+  }
+
+  function hide(el) {
+    el.classList.add('hidden');
   }
 
 });
