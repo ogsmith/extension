bf1f96cfff1d693e3c5e23534dd15d8e0cb78301
Ben Campbell <ben.campbell@americastestkitchen.com>
2016-12-15 15:08:05 -0500

    hit the API YO!

--- api.js
+++ api.js
@@ -1,11 +1,9 @@
-function getHomepage() {
-  console.log("in api.js");
-  $.ajax({
-        type: "GET",
-        dataType: "json",
-	url: "https://www.americastestkitchen.com/api/v4/atk/homepage?site_key=atk",
-	success: function(data){
-	  alert(data);
-	}
-    });
-}()
+(function() {
+  please.get('https://www.americastestkitchen.com/api/v4/atk/homepage?site_key=atk', {
+    success: function(d) {
+      console.log(d);
+    }
+  });
+})();
+
+
