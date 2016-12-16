79eac1a4e49bbcf3935f301f98aafac100d30062
mikehwagz <hmiklw@me.com>
2016-12-16 10:29:44 -0500

    update file structure and refactor css into scss partials

---
+++ assets/js/app.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', _ => {
+
+  $.ajax({
+    url: 'https://www.americastestkitchen.com/api/v4/token',
+    data: '',
+    headers: {
+      "Authorization": "Basic " + btoa('atk' + ":" + 'ezBD2AwjQos63EZbpCUeYAtt')
+    },
+    success: function(d) {
+      hitHomepage();
+    }
+  });
+
+  function hitHomepage() {
+    $.ajax({
+      url: 'https://www.americastestkitchen.com/api/v4/atk/homepage?site_key=atk',
+      data: '',
+      success: function(d) {
+        const recipes = d.data.recipes;
+
+        recipes.pop();
+
+        const template = recipes.map((recipe) => {
+          return `
+            <figure class="recipe">
+              <a class="recipe__image-link" href="https://www.americastestkitchen.com/recipes/${recipe.slug}">
+                <img class="recipe__image" src="http:${recipe.photo.image_url}" alt="${recipe.photo.alt}" />
+              </a>
+              <figcaption class="recipe__data">
+                <a class="recipe__link" href="https://www.americastestkitchen.com/recipes/${recipe.slug}">
+                  <h3 class="recipe__title">${recipe.title}</h3>
+                </a>
+              </figcaption>
+            </figure>`
+        }).join('');
+
+        document.querySelector('.recipes__cards').innerHTML = template;
+      },
+      dataType: 'json'
+    });
+  }
+
+  const form = document.querySelector('.search');
+  const input = document.querySelector('.search__input');
+  const clear = document.querySelector('.search__clear');
+
+  form.addEventListener('submit', searchThis);
+
+  input.addEventListener('keyup', _ => {
+    input.value.length > 0 ? show(clear) : hide(clear);
+  });
+
+  clear.addEventListener('click', _ => {
+    input.value = '';
+    hide(clear)
+  });
+
+  function searchThis(e) {
+    const query = input.value
+    const url = `https://www.americastestkitchen.com/search?q=${query}`;
+
+    chrome.tabs.update(null, {
+        url: url
+    });
+  }
+
+  function show(el) {
+    el.classList.remove('hidden');
+  }
+
+  function hide(el) {
+    el.classList.add('hidden');
+  }
+
+});
